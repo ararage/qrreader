4978110b05e07e3eb958954bd3639775dc08cf14
Ricardo Perez <josericardo.perez.next@bbva.com>
2020-03-10 11:34:04 -0600

    Added QR functions

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,13 +1,87 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+import 'package:barcode_scan/barcode_scan.dart';
+
+import 'package:qrreaderapp/src/pages/direcciones_page.dart';
+import 'package:qrreaderapp/src/pages/mapas_page.dart';
+
+class HomePage extends StatefulWidget {
+  
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int currentIndex = 0;
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child:Text('Hola Home Page'),
+      appBar: AppBar(
+        title: Text("QR Scanner"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.delete_forever),
+            onPressed: (){},
+          )
+        ],
       ),
+      body: Center(
+        child:_callPage(currentIndex),
+      ),
+      bottomNavigationBar: _crearBottomNavigatorBar(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.filter_center_focus),
+        onPressed: (){
+          _scanQR();
+        },
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+    );
+  }
+
+  _scanQR() async {
+    String futureString = '';
+    // Future String : geo:40.70380499375163,-74.01435270747074
+    try{
+      futureString = await BarcodeScanner.scan();
+    }catch(e){
+      futureString = e.toString();
+    }
+    print("Future String : $futureString");
+    if(futureString != null){
+      print("Tenemos informacion");
+    }
+  }
+
+  Widget _callPage(int paginaActual){
+    switch (paginaActual){
+      case 0: return MapasPage();
+      case 1: return DireccionesPage();
+      default: return MapasPage();
+    }
+  }
+
+  Widget _crearBottomNavigatorBar(){
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (index){
+        setState(() {
+          currentIndex = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          title: Text('Mapas')
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.brightness_5),
+          title: Text('Direcciones')
+        )
+      ]
     );
   }
 }
